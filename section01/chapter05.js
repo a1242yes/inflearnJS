0e89016ab7d5bb55558b5e413d9c1797ae2ec04a
a1242yes <a01092211242@gmail.com>
2025-08-22 12:07:25 +0900

    지금까지 한 study

---
+++ section01/chapter05.js
@@ -0,0 +1,40 @@
+// 1.Number Type
+let num1 = 27;
+let num2 = 1.5;
+let num3 = -20;
+
+// console.log(num1 + num2);
+// console.log(num1 - num2);
+// console.log(num1 * num2);
+// console.log(num1 / num2);
+// console.log(num1 % num2);
+
+let inf = Infinity;
+let mInf = -Infinity
+
+let nan = NaN; 
+
+// 2.String Type
+let myName = "양은서";
+let myLocation = "동작"
+const introduce = myName + myLocation;
+
+// console.log(introduce);
+
+// 템플릿 리터럴 문법↓
+let introduceText = `${myName}는 ${myLocation}에 거주`;
+// console.log(introduceText);
+
+// 3.Boolean Type
+let isSwitchOn = true;
+let isEmpty = false;
+
+// 4.Null Type (아무것도 없다.)
+let empty = null;
+
+// 5.Undefined Type
+let none; 
+console.log(none);
+
+
+
---
+++ section01/chapter10.js
@@ -0,0 +1,5 @@
+// 1. for
+
+// for(let idx=0; idx = 100; idx++)
+//     console.log("반복!")
+// }
